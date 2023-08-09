fa9f113093a037caaa63e656f43e1d5a113bf53b
Lexi <76599467+JumpingPistachio@users.noreply.github.com>
2023-08-08 20:22:48 -0400

    Implement driving controls using controller, remove obsolete code

---
+++ src/main/java/frc/robot/Controls.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Controls {
+    public static void controlConfig(RobotContainer bot){
+        bot.drivetrain.setDefaultCommand(bot.drivetrain.run(
+            // CONTROL: Drive - Left Joystick (Y Inverted required)
+            () -> bot.drivetrain.drive(-bot.controller.leftY.getAsDouble(), bot.controller.leftX.getAsDouble())
+            ));
+    }
+}
